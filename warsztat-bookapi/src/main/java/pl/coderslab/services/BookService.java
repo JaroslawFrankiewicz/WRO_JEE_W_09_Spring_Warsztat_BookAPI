bcd4f2ff068fd7c482d71a7603499d6ee8fcf0f5
JaroslawFrankiewicz <frantz12@wp.pl>
2020-09-26 15:28:05 +0200

    BookService

---
+++ warsztat-bookapi/src/main/java/pl/coderslab/services/BookService.java
@@ -0,0 +1,15 @@
+package pl.coderslab.services;
+
+import pl.coderslab.model.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookService {
+    List<Book> getBooks();
+    void add(Book book);
+
+    Optional<Book> findBook(long id);
+}
+
+
